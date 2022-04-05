9adaf8bf531f111979670857ace85c1ca2d034eb
angelrush91 <94041779+angelrush91@users.noreply.github.com>
2022-04-05 22:55:56 +0200

    alle 4 ure virker

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,14 +45,16 @@
             this.Startbutton = new System.Windows.Forms.Button();
             this.buttonUr2 = new System.Windows.Forms.Button();
             this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
-            this.buttonStopUr1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ButtonUr3 = new System.Windows.Forms.Button();
+            this.buttonUr4 = new System.Windows.Forms.Button();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // labelKBH
             // 
             this.labelKBH.AutoSize = true;
-            this.labelKBH.Location = new System.Drawing.Point(69, 25);
+            this.labelKBH.Location = new System.Drawing.Point(69, 34);
             this.labelKBH.Name = "labelKBH";
             this.labelKBH.Size = new System.Drawing.Size(30, 15);
             this.labelKBH.TabIndex = 0;
@@ -61,7 +63,7 @@
             // labelNY
             // 
             this.labelNY.AutoSize = true;
-            this.labelNY.Location = new System.Drawing.Point(69, 75);
+            this.labelNY.Location = new System.Drawing.Point(69, 63);
             this.labelNY.Name = "labelNY";
             this.labelNY.Size = new System.Drawing.Size(57, 15);
             this.labelNY.TabIndex = 1;
@@ -70,7 +72,7 @@
             // labelLN
             // 
             this.labelLN.AutoSize = true;
-            this.labelLN.Location = new System.Drawing.Point(69, 121);
+            this.labelLN.Location = new System.Drawing.Point(69, 92);
             this.labelLN.Name = "labelLN";
             this.labelLN.Size = new System.Drawing.Size(48, 15);
             this.labelLN.TabIndex = 2;
@@ -79,7 +81,7 @@
             // labelTK
             // 
             this.labelTK.AutoSize = true;
-            this.labelTK.Location = new System.Drawing.Point(69, 175);
+            this.labelTK.Location = new System.Drawing.Point(69, 121);
             this.labelTK.Name = "labelTK";
             this.labelTK.Size = new System.Drawing.Size(38, 15);
             this.labelTK.TabIndex = 3;
@@ -88,39 +90,40 @@
             // labelKBHtext
             // 
             this.labelKBHtext.AutoSize = true;
-            this.labelKBHtext.Location = new System.Drawing.Point(219, 30);
+            this.labelKBHtext.Location = new System.Drawing.Point(219, 34);
             this.labelKBHtext.Name = "labelKBHtext";
-            this.labelKBHtext.Size = new System.Drawing.Size(38, 15);
+            this.labelKBHtext.Size = new System.Drawing.Size(27, 15);
             this.labelKBHtext.TabIndex = 4;
-            this.labelKBHtext.Text = "label1";
+            this.labelKBHtext.Text = "SUS";
             this.labelKBHtext.Click += new System.EventHandler(this.labelKBHtext_Click);
             // 
             // labelNewYorktext
             // 
             this.labelNewYorktext.AutoSize = true;
-            this.labelNewYorktext.Location = new System.Drawing.Point(219, 75);
+            this.labelNewYorktext.Location = new System.Drawing.Point(219, 63);
             this.labelNewYorktext.Name = "labelNewYorktext";
-            this.labelNewYorktext.Size = new System.Drawing.Size(38, 15);
+            this.labelNewYorktext.Size = new System.Drawing.Size(27, 15);
             this.labelNewYorktext.TabIndex = 5;
-            this.labelNewYorktext.Text = "label1";
+            this.labelNewYorktext.Text = "SUS";
             // 
             // labelLondontext
             // 
             this.labelLondontext.AutoSize = true;
-            this.labelLondontext.Location = new System.Drawing.Point(219, 121);
+            this.labelLondontext.Location = new System.Drawing.Point(219, 92);
             this.labelLondontext.Name = "labelLondontext";
-            this.labelLondontext.Size = new System.Drawing.Size(38, 15);
+            this.labelLondontext.Size = new System.Drawing.Size(27, 15);
             this.labelLondontext.TabIndex = 6;
-            this.labelLondontext.Text = "label1";
+            this.labelLondontext.Text = "SUS";
+            this.labelLondontext.Click += new System.EventHandler(this.labelLondontext_Click);
             // 
             // labelTokyotext
             // 
             this.labelTokyotext.AutoSize = true;
-            this.labelTokyotext.Location = new System.Drawing.Point(219, 175);
+            this.labelTokyotext.Location = new System.Drawing.Point(219, 121);
             this.labelTokyotext.Name = "labelTokyotext";
-            this.labelTokyotext.Size = new System.Drawing.Size(38, 15);
+            this.labelTokyotext.Size = new System.Drawing.Size(27, 15);
             this.labelTokyotext.TabIndex = 7;
-            this.labelTokyotext.Text = "label1";
+            this.labelTokyotext.Text = "SUS";
             // 
             // timer1
             // 
@@ -130,6 +133,14 @@
             // 
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
             // backgroundWorker1
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
@@ -137,7 +148,7 @@
             // 
             // Startbutton
             // 
-            this.Startbutton.Location = new System.Drawing.Point(310, 276);
+            this.Startbutton.Location = new System.Drawing.Point(373, 26);
             this.Startbutton.Name = "Startbutton";
             this.Startbutton.Size = new System.Drawing.Size(75, 23);
             this.Startbutton.TabIndex = 8;
@@ -147,7 +158,7 @@
             // 
             // buttonUr2
             // 
-            this.buttonUr2.Location = new System.Drawing.Point(310, 305);
+            this.buttonUr2.Location = new System.Drawing.Point(373, 55);
             this.buttonUr2.Name = "buttonUr2";
             this.buttonUr2.Size = new System.Drawing.Size(75, 23);
             this.buttonUr2.TabIndex = 9;
@@ -159,32 +170,41 @@
             // 
             this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
             // 
-            // buttonStopUr1
+            // ButtonUr3
             // 
-            this.buttonStopUr1.Location = new System.Drawing.Point(406, 276);
-            this.buttonStopUr1.Name = "buttonStopUr1";
-            this.buttonStopUr1.Size = new System.Drawing.Size(75, 23);
-            this.buttonStopUr1.TabIndex = 10;
-            this.buttonStopUr1.Text = "Stop ur 1";
-            this.buttonStopUr1.UseVisualStyleBackColor = true;
-            this.buttonStopUr1.Click += new System.EventHandler(this.buttonStopUr1_Click);
+            this.ButtonUr3.Location = new System.Drawing.Point(373, 84);
+            this.ButtonUr3.Name = "ButtonUr3";
+            this.ButtonUr3.Size = new System.Drawing.Size(75, 23);
+            this.ButtonUr3.TabIndex = 10;
+            this.ButtonUr3.Text = "Start ur 3";
+            this.ButtonUr3.UseVisualStyleBackColor = true;
+            this.ButtonUr3.Click += new System.EventHandler(this.ButtonUr3_Click);
             // 
-            // button2
+            // buttonUr4
             // 
-            this.button2.Location = new System.Drawing.Point(406, 305);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonUr4.Location = new System.Drawing.Point(373, 113);
+            this.buttonUr4.Name = "buttonUr4";
+            this.buttonUr4.Size = new System.Drawing.Size(75, 23);
+            this.buttonUr4.TabIndex = 11;
+            this.buttonUr4.Text = "Start ur 4";
+            this.buttonUr4.UseVisualStyleBackColor = true;
+            this.buttonUr4.Click += new System.EventHandler(this.buttonUr4_Click);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.buttonStopUr1);
+            this.Controls.Add(this.buttonUr4);
+            this.Controls.Add(this.ButtonUr3);
             this.Controls.Add(this.buttonUr2);
             this.Controls.Add(this.Startbutton);
             this.Controls.Add(this.labelTokyotext);
@@ -221,7 +241,9 @@
         private System.Windows.Forms.Button Startbutton;
         private System.Windows.Forms.Button buttonUr2;
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
-        private System.Windows.Forms.Button buttonStopUr1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ButtonUr3;
+        private System.Windows.Forms.Button buttonUr4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -23,13 +23,6 @@ namespace verdensur_med_threads
 
         }
 
-        public void timer1_Tick(object sender, EventArgs e)
-        {
-            labelKBHtext.Text = DateTime.Now.AddHours(0).ToString();
-
-            //labelNewYorktext.Text = DateTime.Now.AddHours(-6).ToString();
-            //labelTokyotext.Text = DateTime.Now.AddHours(7).ToString();
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -47,17 +40,66 @@ namespace verdensur_med_threads
             
             timer2.Start();
         }
-
-
-        private void Startbutton_Click(object sender, EventArgs e)
+        public void StartUr3()
         {
-            backgroundWorker1.RunWorkerAsync();
-            
+
+            timer3.Start();
+        }
+        public void StartUr4()
+        {
+
+            timer4.Start();
+        }
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            labelKBHtext.Text = DateTime.Now.AddHours(0).ToString();
+
+            //labelNewYorktext.Text = DateTime.Now.AddHours(-6).ToString();
+            //
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            labelNewYorktext.Text = DateTime.Now.AddHours(1).ToString();
+            labelNewYorktext.Text = DateTime.Now.AddHours(-6).ToString();
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            labelLondontext.Text = DateTime.Now.AddHours(1).ToString();
+        
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            labelTokyotext.Text = DateTime.Now.AddHours(7).ToString();
+        }
+
+        public void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var targetMethod1 = new Action(StartUr1);
+            this.Invoke(targetMethod1);
+            
+        }
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var targetMethod2 = new Action(StartUr2);
+            this.Invoke(targetMethod2);
+        }
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var targetMethod3 = new Action(StartUr3);
+            this.Invoke(targetMethod3);
+        }
+
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var targetMethod4 = new Action(StartUr4);
+            this.Invoke(targetMethod4);
+        }
+        private void Startbutton_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+            
         }
 
         private void buttonUr2_Click(object sender, EventArgs e)
@@ -65,24 +107,22 @@ namespace verdensur_med_threads
             backgroundWorker2.RunWorkerAsync();
         }
 
-        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+
+        private void ButtonUr3_Click(object sender, EventArgs e)
         {
-            var targetMethod = new Action(StartUr2);
-            this.Invoke(targetMethod);
-        }
-        public void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-            var targetMethod = new Action(StartUr1);
-            this.Invoke(targetMethod);
-            
+            backgroundWorker3.RunWorkerAsync();
         }
 
-        private void buttonStopUr1_Click(object sender, EventArgs e)
+        private void buttonUr4_Click(object sender, EventArgs e)
         {
-            backgroundWorker1.CancelAsync();
+            backgroundWorker4.RunWorkerAsync();
         }
-
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void labelLondontext_Click(object sender, EventArgs e)
         {
 
         }
